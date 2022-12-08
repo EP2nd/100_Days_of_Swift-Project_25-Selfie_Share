61a7d4160c651b2c827a1ae9fc70a0de328188b9
EPJr <edwin.przezwiecki.jr@icloud.com>
2022-12-08 02:21:21 +0100

    Clean-ups

--- Project25/ViewController.swift
+++ Project25/ViewController.swift
@@ -4,8 +4,9 @@
 //
 //  Created by Edwin Przeźwiecki Jr. on 27/10/2022.
 //
-import UIKit
+
 import MultipeerConnectivity
+import UIKit
 
 class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
     
@@ -19,15 +20,18 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         super.viewDidLoad()
         
         title = "Selfie Share"
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importImage))
+        
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
         
-        // Challenge 3:
+        /// Challenge 3:
         let listOfConnectionsButton = UIBarButtonItem(title: "Connections", style: .plain, target: self, action: #selector(displayConnections))
-        // Challenge 2:
+        /// Challenge 2:
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
         let broadcastMessageButton = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(broadcastPrompt))
         toolbarItems = [listOfConnectionsButton, spacer, broadcastMessageButton]
+
         navigationController?.isToolbarHidden = false
         
         mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
@@ -39,34 +43,42 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
         
         if let imageView = cell.viewWithTag(1000) as? UIImageView {
             imageView.image = images[indexPath.item]
         }
+        
         return cell
     }
     
     @objc func importImage() {
+        
         let picker = UIImagePickerController()
         picker.allowsEditing = true
         picker.delegate = self
+        
         present(picker, animated: true)
     }
     
     @objc func showConnectionPrompt() {
+        
         let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
         ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
         present(ac, animated: true)
     }
     
-    // Challenge 3:
+    /// Challenge 3:
     @objc func displayConnections() {
+        
         guard let mcSession = mcSession else { return }
         
         let connectedUsers = mcSession.connectedPeers
+        
         var list = ""
         
         if !connectedUsers.isEmpty {
@@ -79,11 +91,13 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         
         let ac = UIAlertController(title: "Connected users", message: "\(list)", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
+        
         present(ac, animated: true)
     }
     
-    // Challenge 2:
+    /// Challenge 2:
     @objc func broadcastPrompt() {
+        
         let ac = UIAlertController(title: "Broadcast", message: "Let others know what is on your mind.", preferredStyle: .alert)
         ac.addTextField()
         ac.addAction(UIAlertAction(title: "Send", style: .default) { [weak self, weak ac] _ in
@@ -91,24 +105,26 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
             self?.broadcastMessage(text)
         })
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
         present(ac, animated: true)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
         guard let image = info[.editedImage] as? UIImage else { return }
         
         dismiss(animated: true)
         
         images.insert(image, at: 0)
-        collectionView.reloadData()
         
-        // Challenge 2:
+        collectionView.reloadData()
+        /// Challenge 2:
         sendImage(image)
     }
     
     // Challenge 2:
     func sendImage(_ image: UIImage) {
-        // 4
+        /// 4. Send it to all peers, ensuring it gets delivered:
         if let imageData = image.pngData() {
             sendData(imageData)
         }
@@ -122,15 +138,15 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     
     // Challenge 2:
     func sendData(_ data: Data) {
-        // 1
+        /// 1. Check if we have an active session we can use:
         guard let mcSession = mcSession else { return }
-        // 2
+        /// 2. Check if there are any peers to send to:
         if mcSession.connectedPeers.count > 0 {
-            // 3
+            /// 3. Convert the new image to a Data object:
             do {
                 try mcSession.send(data, toPeers: mcSession.connectedPeers, with: .reliable)
             } catch {
-                // 5
+                /// 5. Show an error message if there's a problem:
                 let ac = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
                 ac.addAction(UIAlertAction(title: "OK", style: .default))
                 present(ac, animated: true)
@@ -139,15 +155,20 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     }
     
     func startHosting(action: UIAlertAction) {
+        
         guard let mcSession = mcSession else { return }
+        
         mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-project25", discoveryInfo: nil, session: mcSession)
         mcAdvertiserAssistant?.start()
     }
     
     func joinSession(action: UIAlertAction) {
+        
         guard let mcSession = mcSession else { return }
+        
         let mcBrowser = MCBrowserViewController(serviceType: "hws-project25", session: mcSession)
         mcBrowser.delegate = self
+        
         present(mcBrowser, animated: true)
     }
     
@@ -172,23 +193,20 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     }
     
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        
         switch state {
         case.connected:
             print("Connected: \(peerID.displayName)")
-            
         case .connecting:
             print("Connecting: \(peerID.displayName)")
-            
         case .notConnected:
             print("Not Connected: \(peerID.displayName)")
-            
-            // Challenge 1:
+            /// Challenge 1:
             DispatchQueue.main.async {
                 let ac = UIAlertController(title: "Warning", message: "\(peerID.displayName) has disconnected.", preferredStyle: .alert)
                 ac.addAction(UIAlertAction(title: "OK", style: .default))
                 self.present(ac, animated: true)
             }
-            
         @unknown default:
             print("Unknown state received: \(peerID.displayName)")
         }
@@ -199,12 +217,13 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
             if let image = UIImage(data: data) {
                 self?.images.insert(image, at: 0)
                 self?.collectionView.reloadData()
-            
-            // Challenge 2:
+            /// Challenge 2:
             } else {
                 let text = String(decoding: data, as: UTF8.self)
                 let ac = UIAlertController(title: "Message from \(peerID.displayName)", message: text, preferredStyle: .alert)
+                
                 ac.addAction(UIAlertAction(title: "OK", style: .default))
+                
                 self?.present(ac, animated: true)
             }
         }
